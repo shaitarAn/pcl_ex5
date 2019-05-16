446f0cfa18e2f2502c6d45100be97bccf890dbb1
starlordsmom <ashaitar@yahoo.com>
2019-05-16 16:43:21 +0200

    made progress on posterior probability

--- classify.py
+++ classify.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from typing import Dict, Iterable, List
-from math import log
+from math import log, exp
 
 
 class LyricsClassifier:
@@ -13,8 +13,8 @@ class LyricsClassifier:
             train_data: A dict mapping labels to iterables of lines of
                         (e.g. a file object).
         """
-        label_counts = {}
-        label_feature_value_counts = {}
+        self.label_counts = {}
+        self.label_feature_value_counts = {}
         self.line_count = 0
         # store the different seen values for each feature (across the entire
         # training set, independent of label), needed for smoothing
@@ -26,47 +26,69 @@ class LyricsClassifier:
             feature_value_counts = defaultdict(lambda: defaultdict(int))
             for line in lines:
                 self.line_count += 1
-                # print()
-                # print(line.strip())
                 # we look at every line and count how many bobos and mikes
                 label_count += 1
                 # for every line we check if feature is present, count
                 features = self._extract_features(line)
                 for feature_id, value in features.items():
+                    # print(feature_id, value)
                     # feature_id is name of feature
                     # value is True or False; number of what we count
                     # for particular label we count features and count them
                     # we enter info into dict
                     feature_value_counts[feature_id][value] += 1
-                    # for i, v in feature_value_counts.items():
-                    #     print(i, v)
                     # for every feature regardless of label update its set of values
                     feature_values[feature_id].add(value)
-                    # for k, v in feature_values.items():
-                    #     print(k, v)
-            label_counts[label] = label_count
-            # a dict: for every label we get feature and its counts
-            label_feature_value_counts[label] = feature_value_counts
-        # print(label_counts)
-        # print(len(label_feature_value_counts))
-        # for key, value in label_feature_value_counts.items():
-        #     print(key, len(value))
-        #     for k, i in value.items():
-        #         print(key, k, len(i))
-        for label, count in label_counts.items():
-            # total += count
-            print(self._apriori_probability(count))
 
-        print(self.line_count)
+
+            self.label_counts[label] = label_count
+            # a dict: for every label we get feature and its counts
+            self.label_feature_value_counts[label] = feature_value_counts
+
+        print()
+        print('feature_values.items()')
+        for feature, counts in feature_values.items():
+            print(feature, counts)
+
+        print()
+        print('self.label_feature_value_counts.items()')
+        for label, features in self.label_feature_value_counts.items():
+            print()
+            print(label)
+            for f, v in sorted(features.items(), key = lambda v:v[0]):
+                print(f, v)
+        print()
+        for label, count in self.label_counts.items():
+            print("A-priori probability of class {} is {}".format(str(label), self._apriori_probability(count)))
+
+        print("total lines:", self.line_count)
 
         #TODO calculate probabilities
-        # yNB = max(_apriori_probability(label)) + sum(P(feature, label)))
+        # apriori probability per class in logarithmic space
+        self.pBOBO = self._apriori_probability(self.label_counts['bobo'])
+        self.pMIKE = self._apriori_probability(self.label_counts['mike'])
+
+        # raw counts of feature 'grunts' in bobo
+        print('GRUNTS in bobo = {0:.2f}'.format( self.label_feature_value_counts['bobo']['grunts'][True]))
+
+        # posterior (conditional) probability for 'grunts' in 'bobo'
+        # as logarithm
+        # P(Grunts|bobo)
+        GruntsIFbobo = self._aposteriori_probability('grunts', 'bobo')
+
+        # probability of 'bobo' given 'grunts' in non-logarithmic space
+        prob = exp(self.pBOBO)*exp(GruntsIFbobo)
+        # probability of 'bobo' given 'grunts' in logarithmic space
+        p = self.pBOBO+GruntsIFbobo
+        print('probabilityGRUNTS|bobo = {0:.2f}'.format(prob))
+
 
     def _apriori_probability(self, N):
-        return N/self.line_count
+        return float("{0:.2f}".format(log(N/self.line_count)))
+
 
     def _aposteriori_probability(self, f, l):
-        pass
+        return log(self.label_feature_value_counts[l][f][True]/self.label_counts[l])
 
 
     @staticmethod
@@ -82,9 +104,10 @@ class LyricsClassifier:
         #TODO find better features
         return {
             'pray':    'pray' in line,
-            'grunts':  any(grunt in line for grunt in ['oh', 'ah', 'uh']),
-            'tokens':  len(line.split()),
-            'charset': len(set(line)) // 4,
+            'grunts': any(grunt in line for grunt in ['oh', 'ah', 'uh']),
+            'tokens':  len(line.split()) < 6,
+            'areyouready': 'are you ready' in line,
+            'know': 'know' in line,
         }
 
     def _probability(self, label: str, features: Dict) -> float:
